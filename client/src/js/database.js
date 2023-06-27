d465a339e50cfd45e286a21f957e04a0dfadd4f3
tcunningham203 <tcunningham203@gmail.com>
2023-06-28 04:13:03 +0900

    database logic code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,20 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  await store.add(content);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  return store.getAll();
+};
 
 initdb();
+
+export { putDb, getDb };
